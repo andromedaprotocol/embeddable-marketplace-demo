0d822ccedbd9ffd968b15dd4d446acc1aaa120e7
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2022-10-28 23:19:58 +0530

    Added gh-page deploy workflow

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  basePath: process.env.BASE_PATH || "",
+  assetPrefix: process.env.BASE_PATH || "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
