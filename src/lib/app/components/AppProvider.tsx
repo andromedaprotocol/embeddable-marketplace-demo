a1d95be5013e6e76eb27abbe7502f67ca92ef667
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2023-06-27 23:43:20 +0530

    Added guides and small comment on private builds

--- src/lib/app/components/AppProvider.tsx
+++ src/lib/app/components/AppProvider.tsx
@@ -11,6 +11,7 @@ import { AppContext } from "../hooks";
 import { AndromedaProvider } from "@/lib/andrjs";
 import { useAppConfigFromUrl } from "../hooks/useAppConfigFromUrl";
 import Loading from "@/modules/common/ui/Loading";
+// import { APP_ENV } from "@/appEnv"; // Uncomment this line in private build
 
 
 interface AppProviderProps {
@@ -19,10 +20,14 @@ interface AppProviderProps {
 }
 const AppProvider: FC<AppProviderProps> = (props) => {
   const { children, theme = defaultTheme } = props;
-  const { configState } = useAppConfigFromUrl();
+
+  const { configState } = useAppConfigFromUrl(); // Comment this line if you are using your own private build
 
   return (
-    <AppContext.Provider value={{ config: configState as any }}>
+    <AppContext.Provider value={{
+      config: configState // Comment this line if you are using your own private build
+      // config: APP_ENV.DEFAULT_CONFIG // Un-comment this line if you are using your own private build
+    }}>
       <ChakraProvider theme={theme} colorModeManager={ThemeStorageManager}>
         <CSSReset />
         <Loading loading={!configState}>
