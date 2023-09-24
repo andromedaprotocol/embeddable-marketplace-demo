240b3325868c7cd37b558052dde4b576ed4cff1f
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2023-09-24 12:16:21 +0530

    Add all chains db

--- src/appEnv.ts
+++ src/appEnv.ts
@@ -7,6 +7,9 @@ export namespace APP_ENV {
     export const FEATURED_APPS = ['andromeda'];
 
     export const DATABASE: Record<string, string> = {
-        'elgafar-1': "stars1p6wc4rckslpqkffq5sgwtw5umhhu47hqqpdmrmf3cc2cd2tlf47scvls7f",
+        'elgafar-1': 'stars1p6wc4rckslpqkffq5sgwtw5umhhu47hqqpdmrmf3cc2cd2tlf47scvls7f',
+        'galileo-3': 'andr1uaxtg74q2wm2asrnyp3smkaax64zd9krzdk9y58ujfzg33dzzzdsazmf5r',
+        'uni-6': 'juno1n9nyjzaa9yhquhcmqu67wtu8unf2zgly9rnfpm0tddnjuphgydushu5grp',
+        'pisco-1': 'terra1crah72vzxq9a0r7k8xg47xdmr2knd8y5096qrp0fjmsjt95p72sqm7qtny',
     }
 }
