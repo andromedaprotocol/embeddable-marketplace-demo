368e2817db83ce5b18dd2853d5fa6c9b9470d3ba
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2024-04-01 00:48:40 +0530

    Minimize embeddable query load

--- app/[chain]/[app]/[collection]/cw721/[token]/page.tsx
+++ app/[chain]/[app]/[collection]/cw721/[token]/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 import { IAuctionCollection, useAppUtils } from "@/lib/app";
-import { useGetCw721Token } from "@/lib/graphql/hooks/cw721";
 import Cw721TokenPage from "@/modules/cw721/token";
-import { useCodegenGeneratedAdoCw721AndrQuery } from "@andromedaprotocol/gql/dist/__generated/react";
+import { useCodegenGeneratedAdoCw721NftinfoExtensionQuery } from "@andromedaprotocol/gql/dist/__generated/react";
 import { notFound } from 'next/navigation'
 import React, { FC } from "react"
 
@@ -17,9 +16,10 @@ const Page: FC<Props> = (props) => {
     const { params: { collection: collectionId, token: tokenId } } = props;
     const { getCollection } = useAppUtils();
     const collection = getCollection(collectionId) as IAuctionCollection;
-    const { error } = useCodegenGeneratedAdoCw721AndrQuery({
+    const { error } = useCodegenGeneratedAdoCw721NftinfoExtensionQuery({
         variables: {
-            'ADO_cw721_address': collection.cw721
+            'ADO_cw721_address': collection.cw721,
+            'ADO_cw721_cw721_nftInfo_tokenId': tokenId
         }
     });
     console.log(error, "Error");
