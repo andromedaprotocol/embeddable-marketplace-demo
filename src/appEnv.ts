6bc1433e5f3c91a528ce6a5892adee81988fef53
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2024-04-03 16:25:38 +0530

    Add andr devnet embeddable db

--- src/appEnv.ts
+++ src/appEnv.ts
@@ -8,5 +8,6 @@ export namespace APP_ENV {
 
     export const DATABASE: Record<string, string> = {
         'elgafar-1': 'stars1p8w6t6acg9zruu3j5c4qfsp9f96ujkmu33tezeg305qf8lcdhajsdan9ku',
+        'messier-1': 'andr16m5pcz5gpjgnm4drnvaq25uty27nqxplw7q6tesgumsvne9ywcvslswjw6'
     }
 }
