393293052171031b95a2ed5d39f3490970e5544a
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2024-04-04 18:28:34 +0530

    Added local wasm

--- src/appEnv.ts
+++ src/appEnv.ts
@@ -8,6 +8,7 @@ export namespace APP_ENV {
 
     export const DATABASE: Record<string, string> = {
         'elgafar-1': 'stars1p8w6t6acg9zruu3j5c4qfsp9f96ujkmu33tezeg305qf8lcdhajsdan9ku',
-        'messier-1': 'andr16m5pcz5gpjgnm4drnvaq25uty27nqxplw7q6tesgumsvne9ywcvslswjw6'
+        'messier-1': 'andr16m5pcz5gpjgnm4drnvaq25uty27nqxplw7q6tesgumsvne9ywcvslswjw6',
+        'localwasm-1': 'wasm1ftsfd70k99qens0gqzp88c8yj4f2y9tedz2aq0gtuv8grqw93r5qtngvgp'
     }
 }
