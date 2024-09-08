91fa68c755b8538956de2be7095827dd2407432f
Anshudhar Kumar Singh <anshudhar2001@gmail.com>
2024-09-08 18:14:04 +0530

    update: override rc test kernel

--- src/appEnv.ts
+++ src/appEnv.ts
@@ -7,7 +7,6 @@ export namespace APP_ENV {
     export const FEATURED_APPS = ['andromeda'];
 
     export const OVERRIDE_DATABASE: Record<string, string> = {
-        'galileo-4': 'andr1239ga0egsgjzn7w9p5yez7zddzztduq5acfqx6ygxqfeh00y9c0s5h2rhv',
-        'elgafar-1': 'stars15s2f9ycwqjlmeje9jg9muhmpyw62mncw5s74r2tnunq70asqje2szx0glg',
+        'galileo-4': 'andr1a768a990usm3tj5afqwt7t07882vukckcglyvfa34a026e502jvsfmxknv',
     }
 }
